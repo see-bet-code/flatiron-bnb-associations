669ca6926f2eef77a08273313f0f8d0bbe39e078
see-bet-code <brittani.elan.taylor@gmail.com>
2020-10-09 09:29:25 -0400

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
     belongs_to :neighborhood
     belongs_to :host, :class_name => "User"
     has_many :reservations
-    has_many :reviews, through: :guests
+    has_many :reviews, through: :reservations
     has_many :guests, through: :reservations
 
 end
